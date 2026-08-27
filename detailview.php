08c9bb6a0a6510d798924d3b9aff71e073d85cde
Manish Sah <manishsha9939@gmail.com>
2026-08-27 12:56:10 +0530

    fix: revert title in detail view to static 'result view'

--- detailview.php
+++ detailview.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $row['name']; ?></title>
+    <title>result view</title>
     <script src="https://cdn.tailwindcss.com"></script>
 
 </head>
