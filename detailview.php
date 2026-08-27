1207925c10901c5aae86a8a95dac62db70474c61
Manish Sah <manishsha9939@gmail.com>
2026-08-27 13:20:17 +0530

    feat: update detail view to include student roll number, admission number, and date of birth; enhance table layout for academic results

--- detailview.php
+++ detailview.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>result view</title>
     <script src="https://cdn.tailwindcss.com"></script>
-
 </head>
 <body class="bg-[#FFF7EB]" >
     <?php include 'head.php' ?>
@@ -29,61 +28,63 @@
             <h4 class="text-center  text-lg text-gray-800  ">Acadmic Session: 2023-2024</h4>
             <h4 class="text-center font-bold text-lg text-gray-800 ">Student Class: <?php echo $row['class']; ?></h4>
         </div>
-        <div class=" flex justify-center  m-4 p-4  w-full">
-            <div class="">
+        <div class=" flex justify-center  m-4 p-4  ">
+            <div class=" ">
                 <div class=" flex justify-between ">
-            <div class="">
-                <h3>Student Name: <?php echo $row['name']; ?></h3>
-                <h3>Mother's Name: <?php echo $row['mother']; ?></h3>
-                <h3>Father's Name: <?php echo $row['father']; ?></h3>
-                <h3>Address: <?php echo $row['address']; ?></h3>
-                
+                    <div class="">
+                        <h3>Student Name: <?php echo $row['name']; ?></h3>
+                        <h3>Mother's Name: <?php echo $row['mother']; ?></h3>
+                        <h3>Father's Name: <?php echo $row['father']; ?></h3>
+                        <h3>Address: <?php echo $row['address']; ?></h3>
+                    </div>
+                    <div class="">
+                        <h3>Student Roll No: <?php echo $row['roll']; ?></h3>
+                        <h3>Addmission No: <?php echo $row['id']; ?></h3>
+                        <h3>D.O.B: <?php echo $row['dateofbirth']; ?></h3>
+                    </div>
+                </div>
+                <table class="border border-1px border-black">
+                    <tr >
+                        <th class="border border-1px border-black">subject</th>
+                        <th class="border border-1px border-black">min Marks</th>
+                        <th class="border border-1px border-black">Max Marks</th>
+                        <th class="border border-1px border-black">Marks Obtained</th>
+                        <th class="border border-1px border-black">Remark</th>
+                    </tr>
+                    <tr>
+                        <td class="border border-1px border-black">Maths</td>
+                        <td class="border border-1px border-black">35</td>
+                        <td class="border border-1px border-black">100</td>
+                        <td class="border border-1px border-black"><?php echo $row['maths']; ?></td>
+                        <td class="border border-1px border-black"><?php echo ($row['maths'] >= 35) ? 'Pass' : 'Fail'; ?></td>
+                    </tr><tr>
+                        <td class="border border-1px border-black">Science</td>
+                        <td class="border border-1px border-black">35</td>
+                        <td class="border border-1px border-black">100</td>
+                        <td class="border border-1px border-black"><?php echo $row['science']; ?></td>
+                        <td class="border border-1px border-black"><?php echo ($row['science'] >= 35) ? 'Pass' : 'Fail'; ?></td>
+                    </tr><tr>
+                        <td class="border border-1px border-black">English</td>
+                        <td class="border border-1px border-black">35</td>
+                        <td class="border border-1px border-black">100</td>
+                        <td class="border border-1px border-black"><?php echo $row['english']; ?></td>
+                        <td class="border border-1px border-black"><?php echo ($row['english'] >= 35) ? 'Pass' : 'Fail'; ?></td>
+                    </tr><tr>
+                        <td class="border border-1px border-black">Hindi</td>
+                        <td class="border border-1px border-black">35</td>
+                        <td class="border border-1px border-black">100</td>
+                        <td class="border border-1px border-black"><?php echo $row['hindi']; ?></td>
+                        <td class="border border-1px border-black"><?php echo ($row['hindi'] >= 35) ? 'Pass' : 'Fail'; ?></td>
+                    </tr>
+                    <tr>
+                        <?php $total=$row['maths'] + $row['science'] + $row['english'] + $row['hindi']; ?>
+                        <td colspan="1" class="text-center font-bold border border-1px border-black">Total</td>
+                        <td><?php echo $total; ?></td>
+                        <td colspan="1" class="text-center font-bold border border-1px border-black">Percentage</td>
+                        <td><?php echo ($total / 400) * 100 ;?></td>
+                    </tr>
+                </table> 
             </div>
-            <div class="">
-                <h3>Student Roll No: <?php echo $row['roll']; ?></h3>
-                <h3>Addmission No: <?php echo $row['id']; ?></h3>
-                <h3>D.O.B: <?php echo $row['dateofbirth']; ?></h3>
-            </div>
-        </div>
-    <table class="border border-1px border-black">
-        <tr >
-            <th class="border border-1px border-black">subject</th>
-            <th class="border border-1px border-black">min Marks</th>
-            <th class="border border-1px border-black">Max Marks</th>
-            <th class="border border-1px border-black">Marks Obtained</th>
-            <th class="border border-1px border-black">Remark</th>
-        </tr>
-        <tr>
-            <td class="border border-1px border-black">Maths</td>
-            <td class="border border-1px border-black">35</td>
-            <td class="border border-1px border-black">100</td>
-            <td class="border border-1px border-black"><?php echo $row['maths']; ?></td>
-            <td class="border border-1px border-black"><?php echo ($row['maths'] >= 35) ? 'Pass' : 'Fail'; ?></td>
-        </tr><tr>
-            <td class="border border-1px border-black">Science</td>
-            <td class="border border-1px border-black">35</td>
-            <td class="border border-1px border-black">100</td>
-            <td class="border border-1px border-black"><?php echo $row['science']; ?></td>
-            <td class="border border-1px border-black"><?php echo ($row['science'] >= 35) ? 'Pass' : 'Fail'; ?></td>
-        </tr><tr>
-            <td class="border border-1px border-black">English</td>
-            <td class="border border-1px border-black">35</td>
-            <td class="border border-1px border-black">100</td>
-            <td class="border border-1px border-black"><?php echo $row['english']; ?></td>
-            <td class="border border-1px border-black"><?php echo ($row['english'] >= 35) ? 'Pass' : 'Fail'; ?></td>
-        </tr><tr>
-            <td class="border border-1px border-black">Hindi</td>
-            <td class="border border-1px border-black">35</td>
-            <td class="border border-1px border-black">100</td>
-            <td class="border border-1px border-black"><?php echo $row['hindi']; ?></td>
-            <td class="border border-1px border-black"><?php echo ($row['hindi'] >= 35) ? 'Pass' : 'Fail'; ?></td>
-        </tr>
-        <tr>
-            <td colspan="3" class="text-center font-bold border border-1px border-black">Total</td>
-            <td><?php echo $row['maths'] + $row['science'] + $row['english'] + $row['hindi']; ?></td>
-            <td></td>
-    </table> 
-        </div>
         </div>
     
 </body>
